891bdb3566c2f996007e289df1b8a41fda9b51df
Hoang Nguyen <hoangg.dev@gmail.com>
2022-07-27 14:34:57 +0700

    merge dev into master

--- components/SecondaryButton.tsx
+++ components/SecondaryButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Pressable, Text } from 'react-native'
-import { globalStyles } from "../../styles/global";
+import { globalStyles } from '../styles/global'
 
 
 interface SecondaryButtonProps {
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,17 +1,16 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React, { useState } from "react";
-import { globalStyles } from "../styles/global";
 import PrimaryButton from "../components/PrimaryButton";
 import SecondaryButton from "../components/SecondaryButton";
-
+import { globalStyles } from "../styles/global";
 
 const Home = () => {
   const [isPressed, setIsPress] = useState(false)
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home</Text>
-      <PrimaryButton onPress={() => setIsPress(!isPressed)} title={isPressed ? 'Primary Pressed !' : 'Primary'}/>
-      <SecondaryButton onPress={() => setIsPress(!isPressed)} title={isPressed ? 'Secondary Pressed !' : 'Secondary'}/>
+      <PrimaryButton onPress={() => setIsPress(!isPressed)} title={isPressed ? 'Primary Pressed !' : 'Primary'} />
+      <SecondaryButton onPress={() => setIsPress(!isPressed)} title={isPressed ? 'Secondary Pressed !' : 'Secondary'} />
     </View>
   );
 };
